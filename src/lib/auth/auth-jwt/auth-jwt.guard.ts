09869922764699e8dbdde9222b6238d8422b40b5
Daphne-Dev <daphned@naver.com>
2023-09-14 11:04:31 +0900

    fix: AuthJwt payload 타입 추가 및 guard 네이밍 수정

--- src/lib/auth/auth-jwt/auth-jwt.guard.ts
+++ src/lib/auth/auth-jwt/auth-jwt.guard.ts
@@ -1,21 +1,22 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 
 import type { Request } from 'express';
+import type { AuthJwtPayload } from 'src/lib/auth/auth-jwt/auth-jwt.interface';
 
 import { AuthJwtService } from './auth-jwt.service';
 
 @Injectable()
-export class TemporaryJwtGuard implements CanActivate {
+export class AuthJwtGuard implements CanActivate {
   constructor(private authJwtService: AuthJwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest<Request & { user: AuthJwtPayload }>();
     const token = this.extractTokenFromHeader(request);
     if (!token) throw new UnauthorizedException();
 
     try {
       const payload = await this.authJwtService.validationToken(token);
-      request.temporaryUser = payload;
+      request.user = payload;
     } catch (error) {
       throw new UnauthorizedException();
     }
