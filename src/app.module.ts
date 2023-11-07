ce0d1b9be1a2908fad62724447fd0691c5e5c84d
Daphne-dev <daphned@naver.com>
2023-11-07 17:56:22 +0900

    feat: axios 및 전체 에러 처리 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import {
+  BadRequestException,
+  ClassSerializerInterceptor,
+  Module,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { AllExceptionsFilter } from './lib/exception/all-exception.filter';
+import { ERROR_CODE } from './lib/exception/error.constant';
+import { AxiosErrorInterceptor } from './lib/interceptor/axios-error.interceptor';
 
 @Module({
   imports: [
@@ -11,6 +21,32 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        transform: true, // transform payload to DTO
+        forbidUnknownValues: true, // throw error if unknown properties are present
+        exceptionFactory: (errors: ValidationError[]) => {
+          if (!errors[0]?.constraints) return new BadRequestException(ERROR_CODE.INVALID_DATA);
+          const firstKey = Object.keys(errors[0].constraints)[0];
+          throw new BadRequestException({ ...ERROR_CODE.INVALID_DATA, message: errors[0].constraints[firstKey] });
+        },
+      }),
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: AxiosErrorInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
 export class AppModule {}
